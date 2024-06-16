568cc390eb2d7ca04a5b6887e99c0d1cfae5cc56
Tom Calloway <modellurgist@gmail.com>
2024-06-16 20:32:06 +0100

    fix: add missing file

---
+++ lib/sunburn/components/company_total_delivered_power_efficiency.ex
@@ -0,0 +1,7 @@
+defmodule Sunburn.Components.CompanyTotalDeliveredPowerEfficiency do
+  @moduledoc """
+  Documentation for CompanyTotalDeliveredPowerEfficiency components.
+  """
+  use ECSx.Component,
+    value: :float
+end
