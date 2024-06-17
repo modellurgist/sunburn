39d1d8aabd59125741775bc275d5cefe9b4b2761
Tom Calloway <modellurgist@gmail.com>
2024-06-17 08:40:16 +0100

    feat: LiveView display integrated with sampled simulation

--- lib/sunburn_web/live/dashboard.ex
+++ lib/sunburn_web/live/dashboard.ex
@@ -3,15 +3,29 @@ defmodule SunburnWeb.Dashboard do
 
   alias SunburnWeb.StatsCard
 
+  alias Sunburn.Components.{
+    CompanyChangeInTotalDeliveredPower,
+    CompanyTotalDeliveredPowerEfficiency
+  }
+
+  @resample_interval 50
+
   @impl true
   def mount(_params, _session, socket) do
-    values = [
-      %{key: "0", leaf: true, label: "Item 0", data: %{name: "Item 0"}, children: []}
-    ]
+    # values = [
+    #   %{key: "0", leaf: true, label: "Item 0", data: %{name: "Item 0"}, children: []}
+    # ]
+
+    [{company_uuid, _efficiency}] = CompanyTotalDeliveredPowerEfficiency.get_all()
 
     socket =
       socket
-      |> assign(:values, values)
+      |> assign(:company_uuid, company_uuid)
+      |> assign_resampled_company_stats()
+
+    if connected?(socket) do
+      send(self(), :first_sample)
+    end
 
     {:ok, socket}
   end
@@ -22,16 +36,50 @@ defmodule SunburnWeb.Dashboard do
     <div class="w-full grid grid-cols-11">
       <div class="col-start-5 col-span-3">
         <StatsCard.component
-          headline="Successful conversions"
-          status={:good}
-          value={95.0}
+          headline="Power Delivery Efficiency"
+          status={@delivered_power_status}
+          value={@delivered_power_efficiency}
           value_units="%"
-          change_direction={:positive}
-          change_value={5.6}
-          change_units="%"
+          change_direction={@delivered_power_change_direction}
+          change_value={@delivered_power_change}
+          change_units="kWh/day"
         />
       </div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_info(:first_sample, socket) do
+    # :ok = wait_for_spawn(socket.assigns.player_entity)
+
+    # socket =
+    #   socket
+    #   |> assign(loading: false)
+
+    :timer.send_interval(@resample_interval, :resample)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:resample, socket) do
+    {:noreply, assign_resampled_company_stats(socket)}
+  end
+
+  defp assign_resampled_company_stats(socket) do
+    company_uuid = socket.assigns.company_uuid
+
+    power_efficiency = CompanyTotalDeliveredPowerEfficiency.get(company_uuid)
+    power_change = CompanyChangeInTotalDeliveredPower.get(company_uuid)
+    power_change_direction = if power_change >= 0, do: :positive, else: :negative
+
+    delivered_power_status = if power_efficiency >= 98.5, do: :good, else: :bad
+
+    socket
+    |> assign(:delivered_power_status, delivered_power_status)
+    |> assign(:delivered_power_efficiency, power_efficiency)
+    |> assign(:delivered_power_change, power_change)
+    |> assign(:delivered_power_change_direction, power_change_direction)
   end
 end
